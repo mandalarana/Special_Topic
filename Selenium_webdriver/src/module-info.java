304c5d3cde22a73b198b3f9c2bf5c0982b237540
ranap <ranaprathap.venkat@gmail.com>
2023-12-05 19:16:49 -0700

    Selenium_webdriver

---
+++ Selenium_webdriver/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author ranap
+ *
+ */
+module Selenium_webdriver {
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.seleniumhq.selenium.support;
+}
