ec565267f122069018df639e3d10e8254340e43c
ranap <ranaprathap.venkat@gmail.com>
2023-12-07 08:18:31 -0700

    Pagination

---
+++ Pagination/src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author ranap
+ *
+ */
+module Pagination {
+	requires org.seleniumhq.selenium.api;
+	requires dev.failsafe.core;
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.testng;
+}
