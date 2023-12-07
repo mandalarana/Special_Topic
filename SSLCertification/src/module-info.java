9afbe28ab1a8d02081f18468c098d98cbfd481e6
ranap <ranaprathap.venkat@gmail.com>
2023-12-07 07:43:32 -0700

    SSLCertification

---
+++ SSLCertification/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author ranap
+ *
+ */
+module SSLCertification {
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.edge_driver;
+}
