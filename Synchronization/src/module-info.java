750ea877f80dc1b9cd01b784413dbdda3633be21
ranap <ranaprathap.venkat@gmail.com>
2023-12-06 19:00:10 -0700

    Implicit&Explicites

---
+++ Synchronization/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author ranap
+ *
+ */
+module Synchronization {
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.seleniumhq.selenium.support;
+}
