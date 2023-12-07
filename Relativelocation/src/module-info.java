f1ee52aea2d79e585293282af1588f1c937052e3
ranap <ranaprathap.venkat@gmail.com>
2023-12-07 08:35:35 -0700

    Relativelocator

---
+++ Relativelocation/src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author ranap
+ *
+ */
+module Relativelocation {
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.seleniumhq.selenium.support;
+}
