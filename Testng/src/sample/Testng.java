c8e3d4fe13eab2c6e1a7ebb4463fd4ab997681c0
ranap <ranaprathap.venkat@gmail.com>
2023-12-07 09:55:40 -0700

    Sample Testng

---
+++ Testng/src/sample/Testng.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Testng {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	@Test
+public void demo() {
+			System.out.println("test");
+	}
+
+}
